7c873a7d8b851f0207223bc42c7ee967e6f8dbdb
Vivek Yadav <vivek.yadav@riverty.com>
2023-02-04 12:13:02 +0100

    Load articles dynamic
    #14

---
+++ js/dynamic/articles.js
@@ -0,0 +1,108 @@
+/**
+ * File to process the articles dynamically.
+ */
+
+function processData() {
+    for (var i = 0; i < dataArr.length; i++) {
+        var data = dataArr[i];
+        setArticleContainer(data, i)
+    }
+}
+
+function setArticleContainer(data, index) {
+    var mainContainerDiv = $(elem('div')).addClass('col-md-12 border shadow-sm rounded flex-bg')
+    $(mainContainerDiv).attr('id', `article-${index + 1}`)
+    $('#article-data-container').append(mainContainerDiv);
+
+    var outerContainerDiv = $(elem('div')).addClass('row g-0 flex-md-row');
+    $(mainContainerDiv).append(outerContainerDiv);
+
+    var innerContainerDiv = getArticleInnerContainer(data);
+    $(outerContainerDiv).append(innerContainerDiv);
+}
+
+function getArticleInnerContainer(data) {
+    var innerContainerDiv = $(elem('div')).addClass('col-lg-12 pt-4 pt-lg-0 img-text-container');
+
+    var titleInfoContainerDiv = getArticleTitleInfoContainer(data);
+    $(innerContainerDiv).append(titleInfoContainerDiv);
+
+    var titleText = $(elem('h5')).addClass('mb-4 content-title').text(data.title);
+    $(innerContainerDiv).append(titleText);
+
+    var eventImage = $(elem('img')).addClass('float-end imgshadow m-2');
+    $(innerContainerDiv).append(eventImage);
+    $(eventImage).attr('src', data.image).attr('alt', 'Image not found!');
+    $(eventImage).css('width', data.image_width).css('height', data.image_height);
+
+    var eventInfo = $(elem('p')).addClass('card-text mb-4 pt-4').html(data.description);
+    $(innerContainerDiv).append(eventInfo);
+
+    var footerInfoContainerDiv = getArticleFooterInfoContainer(data);
+    $(innerContainerDiv).append(footerInfoContainerDiv);
+
+    return innerContainerDiv;
+}
+
+function getArticleTitleInfoContainer(data) {
+
+    var titleInfoContainerDiv = $(elem('div')).addClass('row mb-4 container-title-info');
+
+    var typeContainerDiv = $(elem('div')).addClass('col-sm-6 mb-3')
+    $(titleInfoContainerDiv).append(typeContainerDiv);
+
+    var typeDiv = $(elem('div')).addClass('container-info-label');
+    $(typeContainerDiv).append(typeDiv);
+
+    var typeIcon = $(elem('i')).addClass('bi bi bi-arrow-right-square-fill');
+    $(typeDiv).append(typeIcon);
+
+    var typeLabel = $(elem('span')).text(' Type');
+    $(typeDiv).append(typeLabel);
+
+    var typeText = $(elem('span')).addClass('container-info-content').text(data.topic);
+    $(typeContainerDiv).append(typeText);
+
+
+    var dateContainerDiv = $(elem('div')).addClass('col-sm-6');
+    $(titleInfoContainerDiv).append(dateContainerDiv);
+
+    var dateDiv = $(elem('div')).addClass('container-info-label');
+    $(dateContainerDiv).append(dateDiv);
+
+    var dateIcon = $(elem('i')).addClass('bi bi bi-calendar3');
+    $(dateDiv).append(dateIcon);
+
+    var dateLabel = $(elem('span')).text(' Date');
+    $(dateDiv).append(dateLabel);
+
+    var dateText = $(elem('span')).addClass('container-info-content').text(data.date);
+    $(dateContainerDiv).append(dateText);
+
+    return titleInfoContainerDiv;
+}
+
+function getArticleFooterInfoContainer(data) {
+
+    var footerInfoContainerDiv = $(elem('div')).addClass('row mb-4 container-title-info');
+
+    var writerContainerDiv = $(elem('div')).addClass('col-sm-6 mb-3');
+    $(footerInfoContainerDiv).append(writerContainerDiv);
+
+    var writerDiv = $(elem('div')).addClass('container-info-label');
+    $(writerContainerDiv).append(writerDiv);
+
+    var writerIcon = $(elem('i')).addClass('bi bi-info-square');
+    $(writerDiv).append(writerIcon);
+
+    var writerLabel = $(elem('span')).text(' Writer');
+    $(writerDiv).append(writerLabel);
+
+    var writerText = $(elem('span')).addClass('container-info-content').text(data.writer);
+    $(writerContainerDiv).append(writerText);
+
+    var writerLink = $(elem('a')).attr('href', data.weblink).attr('target', `_blank`).addClass('content-link').text('Read');
+    $(writerContainerDiv).append(writerLink);
+
+    return footerInfoContainerDiv;
+}
---
+++ js/dynamic/data/articles-data.js
@@ -0,0 +1,133 @@
+var dataArr = [
+  {
+    "title": "Longtermism: the moral significance of future generations",
+    "topic": "Sustainability",
+    "date": "22 Oct 2017",
+    "weblink": "https://80000hours.org/articles/future-generations/?utm_source=google&utm_medium=cpc&gclid=EAIaIQobChMIxMbCpfTy-gIVTPh3Ch0ZkAQoEAAYAiAAEgK3AfD_BwE",
+    "image": "https://80000hours.org/wp-content/uploads/2017/10/longtermism-image-1440x830.png",
+    "image_width": "550px",
+    "image_height": "300px",
+    "writer" : "80000hours.org",
+    "description": `
+      Most people think we should have some concern for future generations,
+      but this obvious sounding idea can leadto a surprising conclusion.
+      <br/>
+      Since the future might be very long, there could be far more people in future generations than in the present generation.
+      This means that if you want to make the world better in 
+      an impartial way — i.e. without regard to people’s race, class, or where or when they’re born —
+      then what most matters morally is that the future goes as well as it can for all generations to come.
+      We’ve called this the ‘long-term value thesis.’
+      <br/>
+      When this thesis is also combined with the idea that some of our actions can have non-negligible effects
+      on how the future goes, it implies that one of our biggest priorities should be ensuring the future goes well.
+      This further idea is usually called ‘longtermism.’
+      Most of this article is about the long-term value thesis, but it comes back to whether we can affect
+      the future at the end.
+      <br/>
+      The long-term value thesis is often confused with the claim that we shouldn’t do anything to help people
+      in the present generation. But the thesis is about what most matters — what we should do about it is a further question.
+      If it turned out that the best way to help those in the future is to improve the lives of people in the present,
+      such as through providing health and education, then longtermists would focus on that.
+      The difference is that the biggest reason to help those in the present would be to improve the long term.
+      <br/>
+      The arguments for and against longtermism are a fascinating new area of research.
+      Many of the key advances have been made by philosophers who have spent time in Oxford, like
+      Derek Parfit, Nick Bostrom, Nick Beckstead, Hilary Greaves and Toby Ord.
+      We’ve found it interesting to watch them deepen and refine these arguments over the last 10 or so years,
+      and we think longtermism might well turn out to be one of the most important discoveries of effective altruism so far.
+      <br/>
+      In the rest of this article, you can see a one-page introduction to longtermism in a nutshell,
+      then we give an overview of the main arguments for and against the long-term value thesis,
+      discuss three common objections, and finish by briefly discussing whether we can affect the future and
+      what all this might imply.
+	`
+  },
+  {
+    "title": "What is a Sustainable Community?",
+    "topic": "Sustainability",
+    "date": "Oct 2022",
+    "weblink": "https://sustain.org/about/what-is-a-sustainable-community/?gclid=Cj0KCQjwpeaYBhDXARIsAEzItbH0rUj1ODMvoBQ-WmW24-QZOfELD7RIE20IYamJ4sPji4sd_cAZZ-kaAsEuEALw_wcB",
+    "image": "https://sustain.org/wp-content/uploads/2018/04/Serbia_GreenList_Parachute-1024x683-1-e1524594104617.jpg",
+    "image_width": "550px",
+    "image_height": "300px",
+    "writer" : "sustain.org",
+    "description": `
+      Throughout the world, people want the same things: access to clean air and water;
+      economic opportunities; a safe and healthy place to raise their kids; shelter; lifelong learning;
+      a sense of community; and the ability to have a say in the decisions that affect their lives.
+      <br/>
+      A sustainable community takes into account, and addresses, multiple human needs,
+      not just one at the exclusion of all others. It is a place where people of diverse backgrounds
+      and perspectives feel welcome and safe, where every group has a seat at the decision-making table,
+      and where prosperity is shared.
+      <br/>
+      It takes a long-term perspective – focusing on anticipating and adapting to change in both the present and future.
+      <br/>
+      A sustainable community manages its human, natural, and financial capital to meet current needs
+      while ensuring that adequate resources are available for future generations.
+	`
+  },
+  {
+    "title": "New global review of regulatory approaches to implement regional action released",
+    "topic": "Pollution",
+    "date": "07 Sep 2022",
+    "weblink": "https://www.indiatoday.in/environment/story/cse-releases-new-global-review-of-regulatory-approaches-to-implement-regional-action-air-quality-1997672-2022-09-07",
+    "image": "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202209/PTI_pollution_india_gate_1200x_1200x768.jpeg",
+    "image_width": "550px",
+    "image_height": "300px",
+    "writer" : "Milan Sharma",
+    "description": `
+      India needs to adopt a regional air quality management framework as cities alone cannot meet their clean air targets
+      if the regional influence of the airshed is not minimised, the CSE emphasised in the review.
+      <br/>
+      On the occasion of the UN International Day of Clean Air for the blue sky,
+      the Centre for Science and Environment (CSE) has warned that air has no boundaries -
+      because of this, clean air action plans that draw hard boundaries around cities for
+      the clean-up job are failing to address the major pollution sources in the larger orbit.
+      They are fighting a losing battle, even as pollution from the larger airshed continues to invade
+      and undermine local efforts.
+      <br/>
+      The airshed concept is when states need to recognise that they share a common air space and
+      that they need to minimise the effect of air pollution spreading from one space into another.
+      <br/>
+      Anumita Roychowdhury, executive director for research and advocacy at the CSE, said,
+      “The science of the regional influence of pollution has begun to take shape in India.
+      The National Clean Air Programme (NCAP) has taken on board the principle of regional air quality”.
+      “But there is no regulatory framework to enable multi-jurisdiction management for aligned action and to
+      establish the upwind and downwind responsibilities of state governments to improve regional air quality.
+      The deadly winter smog that wraps the entire Indo-Gangetic plain every year is a lasting reminder
+      of this regulatory gap”, she added.
+	`
+  },
+  {
+    "title": "Three pioneering cities, one sustainable future",
+    "topic": "Sustainability",
+    "date": "13 Jun 2022",
+    "weblink": "https://www.upm.com/articles/beyond-fossils/22/three-pioneering-cities-one-sustainable-future",
+    "image": "https://www.upm.com/contentassets/03272c736ded46a3811e694c6d3093d0/three-pioneering-cities-main.jpg",
+    "image_width": "550px",
+    "image_height": "300px",
+    "writer" : "Norman Miller, upm.com",
+    "description": `
+      It’s the 2030s. Through bold decisions and inventive technologies, three cities have reached the ambitious climate goals
+      they set a decade earlier. From bike highways to fish lifts, here’s what urban life looks like in the age of kept promises.
+      We picked three cities transforming their cityscapes and living conditions right now – and imagined what life would look
+      like if they played out their pioneering pledges in areas such as water management, energy, and material infrastructure.
+      <br/>
+      Essen becomes a glistening oasis with hi-tech hidden everywhere. "We’re moving away from drainage systems made of concrete
+      and replacing them with infrastructures that mimic natural processes." How can Essen bridge the gap between now and the future?
+      What else is going on in 2030s Essen?
+      <br/>
+      In Nottingham, almost every human act is harnessed to generate energy.
+      How can Nottingham bridge the gap between now and the future? What else is going on in 2030s Nottingham?
+      "In 2021, Nottingham set the most ambitious carbon neutrality commitment of any UK city.
+      With a 2028 deadline, it far outstrips the UK’s national target of net zero by 2050."
+      What else is going on in 2030s Nottingham?
+      <br/>
+      Adelaide tackles deadly heat with sci-fi roads and roofs.
+      "While many European cities are now struggling with deadly heatwaves, Adelaide has found salvation in surprising places:
+      the city’s roads, roofs, and walls." How can Adelaide bridge the gap between now and the future?
+      What else is going on in 2030s Adelaide?
+	`
+  }
+];
